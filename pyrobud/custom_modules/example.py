210cca126e2ec775172a43700b87b547f34841a4
look416 <look416@gmail.com>
2021-01-10 16:39:18 +0000

    Added latest fxparser for flag image detection and gold pairs

--- pyrobud/custom_modules/example.py
+++ pyrobud/custom_modules/example.py
@@ -10,61 +10,19 @@ from .. import command, module, util
 
 
 class ExampleModule(module.Module):
-    name = "Example"
+    name = "Info"
     disabled = False
     channelId = 1179400979
 
     db: util.db.AsyncDB
 
     async def on_load(self) -> None:
-        self.db = self.bot.get_db("example")
+        self.db = self.bot.get_db("info")
 
     async def on_message(self, event: tg.events.NewMessage.Event) -> None:
         if isinstance(event.message.peer_id, PeerChannel) and event.message.peer_id.channel_id == self.channelId:
             self.log.info(f"Received message: {event.message}")
 
-    @command.desc("Simple echo/test command")
-    @command.alias("echotest", "test2")
-    @command.usage("[text to echo?, or reply]", optional=True, reply=True)
-    async def cmd_test(self, ctx: command.Context) -> str:
-        await ctx.respond("Processing...")
-        await asyncio.sleep(1)
-
-        if ctx.input:
-            return ctx.input
-
-        return "It works!"
-
-    async def get_cat(self) -> IO[bytes]:
-        # Get the link to a random cat picture
-        async with self.bot.http.get("https://aws.random.cat/meow") as resp:
-            # Read and parse the response as JSON
-            json = await resp.json()
-            # Get the "file" field from the parsed JSON object
-            cat_url = json["file"]
-
-        # Get the actual cat picture
-        async with self.bot.http.get(cat_url) as resp:
-            # Get the data as a byte array (bytes object)
-            cat_data = await resp.read()
-
-        # Construct a byte stream from the data.
-        # This is necessary because the bytes object is immutable, but we need to add a "name" attribute to set the
-        # filename. This facilitates the setting of said attribute without altering behavior.
-        cat_stream = io.BytesIO(cat_data)
-
-        # Set the name of the cat picture before sending.
-        # This is necessary for Telethon to detect the file type and send it as a photo/GIF rather than just a plain
-        # unnamed file that doesn't render as media in clients.
-        # We abuse pathlib to extract the filename section here for convenience, since URLs are *mostly* POSIX paths
-        # with the exception of the protocol part, which we don't care about here.
-        cat_stream.name = PurePosixPath(cat_url).name
-
-        return cat_stream
-
-    @command.desc("Get a random cat picture")
-    async def cmd_cat(self, ctx: command.Context) -> None:
-        await ctx.respond("Fetching cat...")
-        cat_stream = await self.get_cat()
-
-        await ctx.respond(file=cat_stream, mode="repost")
+    async def on_message_edit(self, event: tg.events.NewMessage.Event) -> None:
+        if isinstance(event.message.peer_id, PeerChannel) and event.message.peer_id.channel_id == self.channelId:
+            self.log.info(f"Received edited message: {event.message}")
