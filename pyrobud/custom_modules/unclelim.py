be6aa1d58d5155ca7c349c514ae2676668e6bf79
look416 <look416@gmail.com>
2021-02-17 10:17:07 +0000

    Added unclelim

---
+++ pyrobud/custom_modules/unclelim.py
@@ -0,0 +1,22 @@
+import asyncio
+import io
+from pathlib import PurePosixPath
+from typing import IO
+import dataclasses as dc
+from .base import BaseModule
+
+import telethon as tg
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel
+import re
+import decimal
+from fxparser import ParserHelper
+
+
+from .. import command, module, util, mt4
+
+
+class UncleLimModule(BaseModule):
+    name = "UncleLim"
+    channelId = 1374877978
+    disabled = False
+    magicNumber = 82704674
