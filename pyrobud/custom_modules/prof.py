7ce5179140166ac2d7986505060e405db5155aff
look416 <look416@gmail.com>
2021-02-21 18:59:44 +0000

    update

--- pyrobud/custom_modules/prof.py
+++ pyrobud/custom_modules/prof.py
@@ -20,21 +20,3 @@ class PPEModule(BaseModule):
     disabled = False
     magicNumber = 82704669
     allowMedia = True
-
-    async def on_message(self, event: tg.events.NewMessage.Event) -> None:
-        if isinstance(event.message.peer_id, PeerChannel) and event.message.peer_id.channel_id == self.channelId:
-            self.log.info(f"Received message: {event.message}")
-            order = self.parseMessage(event.message.message)
-            await self.bot.client.send_message(PeerChannel(channel_id=self.debugChannel), f"Attempted Order {self.name}:{order}")
-            if order.symbol and order.sl:
-                await self.order(order)
-            await self.db.inc("messages_received")
-    
-    async def on_message_edit(self, event: tg.events.NewMessage.Event) -> None:
-        if isinstance(event.message.peer_id, PeerChannel) and event.message.peer_id.channel_id == self.channelId:
-            self.log.info(f"Received edited message: {event.message}")
-            order = self.parseMessage(event.message.message)
-            await self.bot.client.send_message(PeerChannel(channel_id=self.debugChannel), f"Attempted Order {self.name}:{order}")
-            if order.symbol and order.sl:
-                await self.order(order)
-            await self.db.inc("messages_received")
